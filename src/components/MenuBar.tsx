c6ce1d98c925d5111b26c2c58daff79192991860
eightsuzuki <suzukieight8@gmail.com>
2024-07-26 11:48:55 +0900

    Update MenuBar component to use WaveAnimation for background

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,65 +1,115 @@
-import { useState, useEffect } from 'react';
-import { Box, Flex, Link, IconButton, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, useDisclosure } from "@chakra-ui/react";
-import { HamburgerIcon } from '@chakra-ui/icons';
-import WaveAnimation from './WaveAnimation'; // WaveAnimationコンポーネントをインポート
+import { useState, useEffect } from "react";
+import {
+  Box,
+  Flex,
+  Link,
+  IconButton,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import WaveAnimation from "./WaveAnimation"; // WaveAnimationコンポーネントをインポート
 
 const MenuBar = ({ showMenuBar }: { showMenuBar: boolean }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const waves = [
     {
-      y: 20,
+      y: 50,
       length: 0.02,
-      amplitude: 20,
+      amplitude: 5,
       horizontalSpeed: 0.003,
       verticalSpeed: 0.003,
-      color: "rgba(0, 180, 220, 0.5)",
-    }
+      color: "rgba(153, 218, 237, 1)",
+    },
   ];
 
   return (
     <>
-      <Box display={{ base: 'none', md: showMenuBar ? 'block' : 'none' }} position="fixed" top="0" width="100%" bg="rgba(0, 180, 220, 0.5)" zIndex="1000">
-        <Flex justifyContent="center" alignItems="center" py="2"> 
-          <Link href="#home" mx="2" color="white">Top</Link>
-          <Link href="#projects" mx="2" color="white">Projects</Link>
-          <Link href="#skills" mx="2" color="white">Skills</Link>
-          <Link href="#experience" mx="2" color="white">Experience</Link>
-          <Link href="#contact" mx="2" color="white">Contact</Link>
+      <Box
+        display={{ base: "none", md: showMenuBar ? "block" : "none" }}
+        position="fixed"
+        top="0"
+        width="100%"
+        zIndex="1000"
+      >
+        <Flex justifyContent="center" alignItems="center" py="2" >
+          <Link href="#home" mx="2" color="white">
+            Top
+          </Link>
+          <Link href="#projects" mx="2" color="white">
+            Projects
+          </Link>
+          <Link href="#skills" mx="2" color="white">
+            Skills
+          </Link>
+          <Link href="#experience" mx="2" color="white">
+            Experience
+          </Link>
+          <Link href="#contact" mx="2" color="white">
+            Contact
+          </Link>
         </Flex>
-        <Box position="relative" width="100%" height="30px">
-          <WaveAnimation waves={waves} />
-        </Box>
       </Box>
-      <Box display={{ base: showMenuBar ? 'block' : 'none', md: 'none' }} position="fixed" top="0" width="100%" bg="rgba(0, 180, 220, 0.5)" zIndex="1000">
-        <Flex justifyContent="space-between" alignItems="center" px="4"> 
-          <Box color="white" fontWeight="bold">My Portfolio</Box>
+      <Box
+        display={{ base: showMenuBar ? "block" : "none", md: "none" }}
+        position="fixed"
+        top="0"
+        width="100%"
+        zIndex="1000"
+      >
+        <Flex justifyContent="space-between" alignItems="center" px="4">
+          <Box color="white" fontWeight="bold">
+            My Portfolio
+          </Box>
           <IconButton
             icon={<HamburgerIcon />}
             aria-label="Open menu"
             onClick={onOpen}
             color="white"
-            bg="rgb(49, 130, 206)"
-            _hover={{ bg: "rgba(49, 130, 206, 0.8)" }}
+            bg="rgb(49, 130, 20, 0)"
+            _hover={{ bg: "rgba(153, 153, 153, 0.3)" }}
           />
         </Flex>
-        <Drawer placement='left' onClose={onClose} isOpen={isOpen}>
+        <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
           <DrawerOverlay />
           <DrawerContent>
             <DrawerCloseButton />
             <DrawerHeader>Menu</DrawerHeader>
             <DrawerBody>
-              <Link href="#home" display="block" py="2" onClick={onClose}>Top</Link>
-              <Link href="#projects" display="block" py="2" onClick={onClose}>Projects</Link>
-              <Link href="#skills" display="block" py="2" onClick={onClose}>Skills</Link>
-              <Link href="#experience" display="block" py="2" onClick={onClose}>Experience</Link>
-              <Link href="#contact" display="block" py="2" onClick={onClose}>Contact</Link>
+              <Link href="#home" display="block" py="2" onClick={onClose}>
+                Top
+              </Link>
+              <Link href="#projects" display="block" py="2" onClick={onClose}>
+                Projects
+              </Link>
+              <Link href="#skills" display="block" py="2" onClick={onClose}>
+                Skills
+              </Link>
+              <Link href="#experience" display="block" py="2" onClick={onClose}>
+                Experience
+              </Link>
+              <Link href="#contact" display="block" py="2" onClick={onClose}>
+                Contact
+              </Link>
             </DrawerBody>
           </DrawerContent>
         </Drawer>
-        <Box position="relative" width="100%" height="30px">
-          <WaveAnimation waves={waves} />
-        </Box>
+      </Box>
+      <Box
+        display={{base: showMenuBar ? "block" : "none" }}
+        width="100%"
+        height="30px"
+        position="fixed"
+        top="0"
+        zIndex="500"
+      >
+        <WaveAnimation waves={waves} />
       </Box>
     </>
   );
