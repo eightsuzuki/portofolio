77041e74e525d759d7ea638c119bb0a1116015b0
eightsuzuki <suzukieight8@gmail.com>
2024-07-28 18:01:12 +0900

    Update eslint and prettier configurations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,39 @@
-import { useEffect, useState } from "react";
-import { Box, Flex, Text } from "@chakra-ui/react";
-import AOS from "aos";
-import "aos/dist/aos.css"; // AOSのCSSをインポート
-import WaveAnimation from "../components/WaveAnimation";
-import FootstepsIconComponent from "../components/FootstepsTracker";
-import BouncingBox from "../components/BouncingBox";
-import Projects from "../components/Projects";
-import Skills from "../components/Skills";
-import Experience from "../components/Experience";
+import { useEffect, useState } from 'react'
+import { Box, Flex, Text } from '@chakra-ui/react'
+import AOS from 'aos'
+import 'aos/dist/aos.css' // AOSのCSSをインポート
+import WaveAnimation from '../components/WaveAnimation'
+import FootstepsIconComponent from '../components/FootstepsTracker'
+import BouncingBox from '../components/BouncingBox'
+import Projects from '../components/Projects'
+import Skills from '../components/Skills'
+import Experience from '../components/Experience'
 
 export default function Home() {
-  const [windowHeight, setWindowHeight] = useState(0);
-  const [randomText, setRandomText] = useState("");
+  const [windowHeight, setWindowHeight] = useState(0)
+  const [randomText, setRandomText] = useState('')
 
   useEffect(() => {
-    setWindowHeight(window.innerHeight);
+    setWindowHeight(window.innerHeight)
 
     const handleResize = () => {
-      setWindowHeight(window.innerHeight);
-    };
+      setWindowHeight(window.innerHeight)
+    }
 
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize)
 
-    AOS.init({ duration: 1000 }); // AOSの初期化
+    AOS.init({ duration: 1000 }) // AOSの初期化
 
     return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   useEffect(() => {
-    const texts = ["Hello...", "Welcome!", "Hi there!", "Howdy!", "I`m 8", "suzuki8"];
-    const randomIndex = Math.floor(Math.random() * texts.length);
-    setRandomText(texts[randomIndex]);
-  }, []);
+    const texts = ['Hello...', 'Welcome!', 'Hi there!', 'Howdy!', 'I`m 8', 'suzuki8']
+    const randomIndex = Math.floor(Math.random() * texts.length)
+    setRandomText(texts[randomIndex])
+  }, [])
 
   const waves = [
     {
@@ -42,7 +42,7 @@ export default function Home() {
       amplitude: 40,
       horizontalSpeed: 0.001,
       verticalSpeed: 0.001,
-      color: "rgba(0, 119, 190, 0.5)",
+      color: 'rgba(0, 119, 190, 0.5)',
     },
     {
       y: windowHeight * 0.7,
@@ -50,7 +50,7 @@ export default function Home() {
       amplitude: 30,
       horizontalSpeed: 0.002,
       verticalSpeed: 0.002,
-      color: "rgba(0, 150, 200, 0.5)",
+      color: 'rgba(0, 150, 200, 0.5)',
     },
     {
       y: windowHeight * 0.8,
@@ -58,56 +58,56 @@ export default function Home() {
       amplitude: 20,
       horizontalSpeed: 0.003,
       verticalSpeed: 0.003,
-      color: "rgba(0, 180, 220, 0.5)",
+      color: 'rgba(0, 180, 220, 0.5)',
     },
-  ];
+  ]
 
   return (
     <Box>
-      <Box w="100%" h="100%" bg="#faf3e0">
-        <Box position="relative">
+      <Box w='100%' h='100%' bg='#faf3e0'>
+        <Box position='relative'>
           <FootstepsIconComponent />
           <Flex
-            id="home"
-            direction="column" // 縦方向に並べる
-            h="100vh"
-            alignItems="center"
-            justifyContent="center"
-            position="relative"
+            id='home'
+            direction='column' // 縦方向に並べる
+            h='100vh'
+            alignItems='center'
+            justifyContent='center'
+            position='relative'
           >
             <Box
-              position="absolute"
-              display="flex"
-              flexDirection="column"
-              alignItems="center"
-              justifyContent="center"
-              textAlign="center"
-              maxWidth="100%"
-              overflow="hidden"
+              position='absolute'
+              display='flex'
+              flexDirection='column'
+              alignItems='center'
+              justifyContent='center'
+              textAlign='center'
+              maxWidth='100%'
+              overflow='hidden'
             >
               <BouncingBox
                 text={randomText} // ランダムなテキストを表示
-                fontSize="50px"
-                color="white"
-                mx="8px"
+                fontSize='50px'
+                color='white'
+                mx='8px'
               />
             </Box>
             <WaveAnimation waves={waves} />
           </Flex>
-          <Flex direction="column" alignItems="center" data-aos="fade-up">
-            <Box id="about" data-aos="fade-up"></Box>
-            <Box id="projects" data-aos="fade-up">
+          <Flex direction='column' alignItems='center' data-aos='fade-up'>
+            <Box id='about' data-aos='fade-up'></Box>
+            <Box id='projects' data-aos='fade-up'>
               <Projects />
             </Box>
-            <Box id="skills" data-aos="fade-up">
+            <Box id='skills' data-aos='fade-up'>
               <Skills />
             </Box>
-            <Box id="experience" data-aos="fade-up">
+            <Box id='experience' data-aos='fade-up'>
               <Experience />
             </Box>
           </Flex>
         </Box>
       </Box>
     </Box>
-  );
+  )
 }
