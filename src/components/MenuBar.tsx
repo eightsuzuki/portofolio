b1c650d927c401941db56c60d93d01a7466032a9
eightsuzuki <suzukieight8@gmail.com>
2024-08-12 00:34:44 +0900

    Add pointerEvents to click outside of drawer to close

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -11,134 +11,157 @@ import {
   DrawerBody,
   useDisclosure,
   Text,
-} from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
-import WaveAnimation from "./WaveAnimation"; // WaveAnimationコンポーネントをインポート
-import { useState, useEffect } from "react";
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
+import WaveAnimation from './WaveAnimation' // WaveAnimationコンポーネントをインポート
+import { useState, useEffect } from 'react'
 
 const MenuBar = ({ showMenuBar }: { showMenuBar: boolean }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [transform, setTransform] = useState("translateY(-100%)");
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [transform, setTransform] = useState('translateY(-100%)')
 
   useEffect(() => {
     if (showMenuBar) {
-      setTransform("translateY(0)");
+      setTransform('translateY(0)')
     } else {
-      setTransform("translateY(-100%)");
+      setTransform('translateY(-100%)')
     }
-  }, [showMenuBar]);
+  }, [showMenuBar])
 
-  const waves = [
+  const wave1 = [
     {
       y: 50,
       length: 0.02,
       amplitude: 5,
       horizontalSpeed: 0.003,
       verticalSpeed: 0.003,
-      color: "rgba(61, 172, 212, 1)",
+      color: 'rgba(61, 172, 212, 1)',
     },
-  ];
+  ]
+
+  const wave2 = [
+    {
+      y: 500,
+      length: 0.02,
+      amplitude: 5,
+      horizontalSpeed: 0.003,
+      verticalSpeed: 0.003,
+      color: 'rgba(61, 172, 212, 1)',
+    },
+  ]
 
   return (
     <>
+      {/* MenuBar と WaveAnimation のスライドイン部分はそのまま */}
       <Box
-        display={{ base: "none", md: "block" }}
-        position="fixed"
-        top="0"
-        width="100%"
-        zIndex="1000"
+        display={{ base: 'none', md: 'block' }}
+        position='fixed'
+        top='0'
+        width='100%'
+        zIndex='1000'
         transform={transform}
-        transition="transform 1s ease-in-out"
+        transition='transform 0.3s ease-in-out'
       >
-        <Flex
-          justifyContent="space-between"
-          alignItems="center"
-          px="6"
-          color="white"
-          height="100%"
-        >
-          <Text fontWeight="bold">
+        <Flex justifyContent='space-between' alignItems='center' px='6' color='white' height='100%'>
+          <Text fontWeight='bold'>
             <Link
-              href="/"
-              mx="4"
-              color="white"
-              fontSize="3xl"
-              textDecoration="none"
-              _hover={{ textDecoration: "none" }}
+              href='/'
+              mx='4'
+              color='white'
+              fontSize='3xl'
+              textDecoration='none'
+              _hover={{ textDecoration: 'none' }}
             >
-            suzuki8
+              Suzuki8
             </Link>
           </Text>
-          <Flex justifyContent="center" alignItems="center">
-            <Link href="#home" mx="3" fontSize="xl">
+          <Flex justifyContent='center' alignItems='center'>
+            <Link href='#home' mx='3' fontSize='xl'>
               Top
             </Link>
-            <Link href="#projects" mx="3" fontSize="xl">
+            <Link href='#projects' mx='3' fontSize='xl'>
               Projects
             </Link>
-            <Link href="#skills" mx="3" fontSize="xl">
+            <Link href='#skills' mx='3' fontSize='xl'>
               Skills
             </Link>
-            <Link href="#experience" mx="3" fontSize="xl">
+            <Link href='#experience' mx='3' fontSize='xl'>
               Experience
             </Link>
-            <Link href="#contact" mx="3" fontSize="xl">
+            <Link href='#contact' mx='3' fontSize='xl'>
               Contact
             </Link>
           </Flex>
         </Flex>
       </Box>
       <Box
-        display={{ base: "block", md: "none" }}
-        position="fixed"
-        top="0"
-        width="100%"
-        zIndex="1000"
+        display={{ base: 'block', md: 'none' }}
+        position='fixed'
+        top='0'
+        width='100%'
+        zIndex='1000'
         transform={transform}
-        transition="transform 1s ease-in-out"
+        transition='transform 0.3s ease-in-out'
       >
-        <Flex justifyContent="space-between" alignItems="center" px="6">
-          <Text fontWeight="bold">
+        <Flex justifyContent='space-between' alignItems='center' px='6'>
+          <Text fontWeight='bold'>
             <Link
-              href="/"
-              mx="4"
-              color="white"
-              fontSize="3xl"
-              textDecoration="none"
-              _hover={{ textDecoration: "none" }}
+              href='/'
+              mx='4'
+              color='white'
+              fontSize='3xl'
+              textDecoration='none'
+              _hover={{ textDecoration: 'none' }}
             >
               Suzuki8
             </Link>
           </Text>
           <IconButton
             icon={<HamburgerIcon w={8} h={8} />}
-            aria-label="Open menu"
+            aria-label='Open menu'
             onClick={onOpen}
-            color="white"
-            bg="rgb(49, 130, 20, 0)"
-            _hover={{ bg: "rgba(153, 153, 153, 0.3)" }}
-            size="lg"
+            color='white'
+            bg='rgb(49, 130, 20, 0)'
+            _hover={{ bg: 'rgba(153, 153, 153, 0.3)' }}
+            size='lg'
           />
         </Flex>
-        <Drawer placement="top" onClose={onClose} isOpen={isOpen}>
+        <Drawer placement='top' onClose={onClose} isOpen={isOpen}>
           <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader>Menu</DrawerHeader>
-            <DrawerBody>
-              <Link href="/" display="block" py="2" onClick={onClose}>
+          <DrawerContent position='relative'>
+            <Box
+              position='fixed'
+              top='0'
+              zIndex='1'
+              pointerEvents='none'
+              transition='transform 0.3s ease-in-out'
+            >
+              <WaveAnimation waves={wave2} />
+            </Box>
+            <DrawerCloseButton
+              color='white'
+              size='lg'
+              position='absolute'
+              bottom='20px'
+              transform='translateX(-50%)'
+            />
+            <DrawerHeader color='white' zIndex='1'>
+              Menu
+            </DrawerHeader>
+            <DrawerBody zIndex='1'>
+              <Link href='/' display='block' py='2' color='white' onClick={onClose}>
                 Top
               </Link>
-              <Link href="#projects" display="block" py="2" onClick={onClose}>
+              <Link href='#projects' display='block' py='2' color='white' onClick={onClose}>
                 Projects
               </Link>
-              <Link href="#skills" display="block" py="2" onClick={onClose}>
+              <Link href='#skills' display='block' py='2' color='white' onClick={onClose}>
                 Skills
               </Link>
-              <Link href="#experience" display="block" py="2" onClick={onClose}>
+              <Link href='#experience' display='block' py='2' color='white' onClick={onClose}>
                 Experience
               </Link>
-              <Link href="#contact" display="block" py="2" onClick={onClose}>
+              <Link href='#contact' display='block' py='2' color='white' onClick={onClose}>
                 Contact
               </Link>
             </DrawerBody>
@@ -146,20 +169,18 @@ const MenuBar = ({ showMenuBar }: { showMenuBar: boolean }) => {
         </Drawer>
       </Box>
       <Box
-        display={{ base: "block" }}
-        width="100%"
-        height="30px"
-        position="fixed"
-        top="0"
-        zIndex="500"
-        pointerEvents="none"
+        display={{ base: 'block' }}
+        position='fixed'
+        top='0'
+        zIndex='500'
+        pointerEvents='none'
         transform={transform}
-        transition="transform 1s ease-in-out"
+        transition='transform 0.3s ease-in-out'
       >
-        <WaveAnimation waves={waves} />
+        <WaveAnimation waves={wave1} />
       </Box>
     </>
-  );
-};
+  )
+}
 
-export default MenuBar;
+export default MenuBar
